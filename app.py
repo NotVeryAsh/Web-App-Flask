ea6b2d252405d41b8441842950fe5c12e9d3e2a3
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2022-11-17 19:34:20 +0000

    Adds basic authentication system

--- app.py
+++ app.py
@@ -1,39 +1,64 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, request, render_template, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html', title='Example Flask Web App')
 
 
-@app.route("/blog/<string:post_title>")
-def view_blog_post(post_title):
-    return f"<h2>You are viewing Blog Post: '{escape(post_title)}'</h2>"
+@app.get('/profile/')
+def profile_dashboard():
+    return render_template('/profile/dashboard.html')
 
 
-@app.route("/order/<int:record_id>")
-def view_record(record_id):
-    return f"Found record with id '{escape(record_id)}"
+@app.get('/login/')
+def show_login():
+    if 'username' in session:
+        return redirect(url_for('profile_dashboard'))
+    error = None
+    return render_template('login.html', error=error)
 
 
-@app.route("/user/<uuid:user_id>")
-def view_user(user_id):
-    return f"Found user with id '{escape(user_id)}"
+@app.post('/login/')
+def login():
+
+    username = None
+
+    if 'username' in request.form and 'password' in request.form:
+
+        username = request.form['username'].lstrip().lower()
+        password = request.form['password']
+
+        if validate_login_credentials(username, password):
+            session['username'] = username
+            session['password'] = password
+
+            return redirect(url_for('profile_dashboard'))
+        else:
+            flash('The credentials you provided are invalid')
+            return redirect(url_for('show_login'))
+
+    if username is None:
+        flash('The username field is required')
+    else:
+        flash('The password field is required')
+
+    return redirect(url_for('show_login'))
 
 
-@app.route("/float/<float:number>")
-def view_float(number):
-    return f"This float is '{escape(number)}"
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect(url_for('index'))
 
 
-@app.route("/breadcrumb/<path:slug>")
-def breadcrumb(slug):
-    return f"Resources found at path '{escape(slug)}"
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/page_not_found.html'), 404
 
 
-@app.route("/<name>")
-def hello(name):
-    return f"The page '{escape(name)}' could not be found"
+def validate_login_credentials(username, password):
+    return username == 'ash' and password == 'password'
